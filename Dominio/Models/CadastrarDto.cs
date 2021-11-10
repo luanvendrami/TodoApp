ab60fb23512acfc0272a22c9a9ba0201205c9419
luanvendrami <luan.vendrami@hotmail.com>
2021-11-10 01:30:42 -0300

    Dto para passar os parametros do swagger para o back-end, principalmente para os metodos de post, put e remove.

---
+++ Dominio/Models/CadastrarDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Models
+{
+    public class CadastrarDto
+    {
+        public string Nome { get; set; }
+        public DateTime DataConclusao { get; set; }
+        public bool TarefaConcluida { get; set; }
+    }
+}
