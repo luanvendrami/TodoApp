82435a2fe044908010306868039f2dad374e9b0e
luanvendrami <luan.vendrami@hotmail.com>
2021-11-10 01:29:50 -0300

    Criação do service onde irá conter os metodos da aplicação e sua interface para ser injetada no controller e demais locais.

--- Dominio/Interfaces/Services/TodoAppService/ITodoAppService.cs
+++ Dominio/Interfaces/Services/TodoAppService/ITodoAppService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dominio.Entidades;
+using Dominio.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,8 @@ namespace Dominio.Interfaces.Repositorio.TodoAppService
 {
     public interface ITodoAppService
     {
+        void SalvarCadastro(CadastrarDto model);
+        List<CadastroEntidades> BuscarPorDataConclusao();
+        void AlterarValorTarefa(Guid id, bool tarefa);
     }
 }
--- Service/TodoAppService.cs
+++ Service/TodoAppService.cs
@@ -1,9 +1,56 @@
 ﻿
+using Dominio.Entidades;
+using Dominio.Interfaces.Repositorio.TodoAppRepositorio;
 using Dominio.Interfaces.Repositorio.TodoAppService;
+using Dominio.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Service
 {
     public class TodoAppService : ITodoAppService
     {
+        private readonly ICadastroRepositorio _cadastroRepositorio;
+
+        public TodoAppService(ICadastroRepositorio cadastroRepositorio)
+        {
+            _cadastroRepositorio = cadastroRepositorio;
+        }
+
+        public void SalvarCadastro(CadastrarDto model)
+        {
+            if (model.DataConclusao >= DateTime.Now)
+            {
+                var entidadeCadastro = new CadastroEntidades
+                {
+                    Nome = model.Nome,
+                    DataConclusao = model.DataConclusao,
+                    TarefaConcluida = model.TarefaConcluida
+                };
+                _cadastroRepositorio.Adicionar(entidadeCadastro);
+            }
+
+            _cadastroRepositorio.SalvarAlteracao();
+        }
+
+        public List<CadastroEntidades> BuscarPorDataConclusao()
+        {
+            var consultaRepositorio = _cadastroRepositorio.RetornaListaDataConclusao();
+            return consultaRepositorio;  
+        }
+
+        public void AlterarValorTarefa(Guid id, bool tarefa)
+        {
+
+            var consultaRepositorio = _cadastroRepositorio.RetornaId(id);
+            if (consultaRepositorio.TarefaConcluida != tarefa)
+            {
+                consultaRepositorio.TarefaConcluida = tarefa;
+                _cadastroRepositorio.Atualizar(consultaRepositorio);
+            }
+
+            _cadastroRepositorio.SalvarAlteracao();
+        }
     }
 }
